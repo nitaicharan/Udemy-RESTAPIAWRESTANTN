e0cd23ff2bafefd0ab2ca8666b1845a450572d67
Nitai Charan <nitaicharan@gmail.com>
2020-10-30 17:59:23 -0300

    45. How to extract JSON response - Part 1

---
+++ src/test/java/com/nitaicharan/udemy_restapiawrestantn1/twitterapiexemple/TwitterExtractResponsetTest.java
@@ -0,0 +1,44 @@
+package com.nitaicharan.udemy_restapiawrestantn1.twitterapiexemple;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class TwitterExtractResponsetTest {
+
+    private String TWITTER_KEY = System.getenv("TWITTER_KEY");
+    private String TWITTER_SECRET_KEY = System.getenv("TWITTER_SECRET_KEY");
+    private String TWITTER_TOKEN = System.getenv("TWITTER_TOKEN");
+    private String TWITTER_SECRET_TOKEN = System.getenv("TWITTER_SECRET_TOKEN");
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://api.twitter.com";
+        RestAssured.basePath = "/1.1/statuses";
+    }
+
+    @Test
+    public void keyNotNull() {
+        assertNotNull(TWITTER_KEY);
+        assertNotNull(TWITTER_SECRET_KEY);
+        assertNotNull(TWITTER_TOKEN);
+        assertNotNull(TWITTER_SECRET_TOKEN);
+    }
+
+    @Test
+    public void statusCode200AndReponseNotNullVerification() {
+        var response = RestAssured.given()//
+                .auth()//
+                .oauth(TWITTER_KEY, TWITTER_SECRET_KEY, TWITTER_TOKEN, TWITTER_SECRET_TOKEN)//
+                .queryParam("status", "My First Tweet")//
+                .when()//
+                .post("/update.json").then()//
+                .statusCode(200)//
+                .extract().response();
+
+        assertNotNull(response.path("id_str"));
+    }
+}
