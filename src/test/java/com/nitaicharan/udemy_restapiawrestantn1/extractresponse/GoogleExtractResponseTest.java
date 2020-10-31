7123676d743cddbd7224265f98886185f6004421
Nitai Charan <nitaicharan@gmail.com>
2020-10-31 19:16:58 -0300

    51. How to extract XML response - Part 2

--- src/test/java/com/nitaicharan/udemy_restapiawrestantn1/extractresponse/GoogleExtractResponseTest.java
+++ src/test/java/com/nitaicharan/udemy_restapiawrestantn1/extractresponse/GoogleExtractResponseTest.java
@@ -1,12 +1,14 @@
 package com.nitaicharan.udemy_restapiawrestantn1.extractresponse;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.path.xml.XmlPath;
 
 public class GoogleExtractResponseTest {
     private String KEY = System.getenv("GOOGLEMAP_KEY");
@@ -34,5 +36,9 @@ public class GoogleExtractResponseTest {
                 .then()//
                 .statusCode(200).extract().response();
         assertTrue(response.contentType().contains("application/xml"));
+
+        var xmlPaht = new XmlPath(response.asInputStream());
+        var value = xmlPaht.get("distancematrixresponce.row.element.duration.value");
+        assertEquals("13770", value);
     }
 }
